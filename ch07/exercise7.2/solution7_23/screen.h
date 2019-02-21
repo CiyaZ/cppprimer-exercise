50de5f90e362c9480af97135831faab3923066a5
CiyaZ <ciyaz@qq.com>
2019-02-21 22:25:45 +0800

    exercise7.2

---
+++ ch07/exercise7.2/solution7_23/screen.h
@@ -0,0 +1,45 @@
+//
+// Created by gaoshuhang on 2019/2/21.
+//
+
+#ifndef DEMO01_SCREEN_H
+#define DEMO01_SCREEN_H
+
+#include <string>
+
+class Screen
+{
+public:
+	typedef std::string::size_type pos;
+
+	Screen() = default;
+
+	Screen(pos ht, pos wd) : height(ht), width(wd), contents(ht * width, '')
+	{};
+
+	Screen(pos ht, pos wd, char c) : height(ht), width(wd), contents(ht * wd, c)
+	{};
+
+	inline char get() const
+	{
+		return contents[cursor];
+	}
+
+	inline char get(pos ht, pos wd) const
+	{
+		return contents[ht * width + wd];
+	}
+
+	inline Screen &move(pos ht, pos wd)
+	{
+		this->cursor = ht * width + wd;
+		return *this;
+	}
+
+private:
+	pos cursor = 0;
+	pos height = 0, width = 0;
+	std::string contents = "";
+};
+
+#endif //DEMO01_SCREEN_H
